d95f15479848677b0981782f26a3d1b787e33904
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2021-11-23 18:00:46 +0600

    test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,25 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
+    public float maxVelocity;
+    public float acceleration;
+    public float jumpStrength;
+    public Rigidbody2D body;
+    public Collider2D playerCollider;
+
+    private bool isJumping = false;
+
     private Vector2 directionalInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        throw new NotImplementedException();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 movement = directionalInput * acceleration * Time.fixedDeltaTime;
+        MovePlayer(movement);
     }
 
     public void OnJumpPressed()
     {
-        
+        if (!isJumping)
+        {
+            isJumping = true;
+            body.AddForce(jumpStrength * Vector2.up);
+        }
     }
 
     public void OnJumpReleased()
@@ -30,5 +51,13 @@ public class Player : MonoBehaviour
     public void SetDirectionalInput(Vector2 input)
     {
         directionalInput = input;
+    }
+
+    private void MovePlayer(Vector2 movement)
+    {
+        if (body.velocity.x * acceleration <= maxVelocity)
+        {
+            body.AddForce(movement);            
+        }
     }
 }
